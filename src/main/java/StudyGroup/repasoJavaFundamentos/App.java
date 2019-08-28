5e3e21a5612b9a1880b15dab72790377dd605e93
teban25 <exteban.1225@gmail.com>
2019-08-27 21:03:58 -0500

    Creacion del proyecto

---
+++ src/main/java/StudyGroup/repasoJavaFundamentos/App.java
@@ -0,0 +1,13 @@
+package StudyGroup.repasoJavaFundamentos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
